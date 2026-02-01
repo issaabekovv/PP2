6c874749125f52a01b55df29bb37f17f537d3f26
Dulat Isabek <d_isabek@kbtu.kz>
2026-02-01 23:47:02 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ Practice 2/boolean/boolean_operators.py
@@ -0,0 +1,13 @@
+print(bool("Hello"))
+print(bool(15))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+
+print(x is not y)
---
+++ Practice 2/if_else/if_else.py
@@ -0,0 +1,13 @@
+number = 7
+
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
