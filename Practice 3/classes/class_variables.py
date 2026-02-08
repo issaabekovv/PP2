1e0a5816b751d62471149277fcb2859162bf40c5
Dulat <d_isabek@kbtu.kz>
2026-02-08 22:54:12 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

---
+++ Practice 3/classes/class_variables.py
@@ -0,0 +1,24 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def greet(self):
+    print("Hello, my name is " + self.name)
+
+p1 = Person("Emil", 25)
+p1.greet()
+
+
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def printname(self):
+    print(self.name)
+
+p1 = Person("Tobias")
+p2 = Person("Linus")
+
+p1.printname()
+p2.printname()
